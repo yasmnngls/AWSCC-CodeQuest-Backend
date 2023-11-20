949e58a2cc8095219d3af6f8804b222e21124c21
yasmnngls <yasmnngls@users.noreply.github.com>
2023-11-20 15:28:51 +0800

    Info about me
    -> Using the print function
    -> Using escape sequence
    -> Changing variables

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,5 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "Yhasmen Nogales"
+department = "Software and Web Development"
+funfact = "my mbti is Infj - A" 
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"Hello, I am {name}. I love {department} and I love being in the Backend even more!\nOne thing about me is that {funfact}")
