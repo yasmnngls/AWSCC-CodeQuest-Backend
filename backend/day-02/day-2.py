0e65bf1befe15863fed6bbf44c28b081367ec697
yasmnngls <yasmnngls@users.noreply.github.com>
2023-11-21 12:58:11 +0800

    User Input
    -> Simple calculator that adds three numbers from the user

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,8 @@
+#using input() to take user input 
+Num1 = int(input ("Enter the first number: "))
+Num2 =int (input ("Enter the second number: "))
+Num3 = int(input("Enter the third number: "))
+
+#For sum 
+result = Num1 + Num2 + Num3
+print ("Sum: ", result)
