d926b4ed8999a9c1b6e5bdf5558e0c952fc49f7b
yasmnngls <yasmnngls@users.noreply.github.com>
2023-11-23 08:50:07 +0800

    Mini Challenge
    using conditional statements

---
+++ backend/day-03/day-3.py
@@ -0,0 +1,8 @@
+Num1 = int(input("Enter a number: "))
+
+if Num1 < 0:
+    print(f"{Num1} is a Negative Number!")
+elif Num1 > 0:   
+    print(f"{Num1} is a Positive Number!")
+else: 
+    print("That number is a Zero!")
